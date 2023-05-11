85ac4d3f1b0356079669bfe15ebcc830b9c0a527
wsbgtzh <tangzuhan@gmail.com>
2023-05-11 22:57:12 +0800

    kof

---
+++ static/js/player/kyo.js
@@ -0,0 +1,37 @@
+import { Player } from "./base.js";
+import { GIF } from "../utils/gif.js";
+
+export class Kyo extends Player {
+    constructor(root, info) {
+        super(root, info);
+
+        this.init_animations();
+    }
+
+    init_animations() {
+        let outer = this;
+        let offsets = [0, -22, -22, -140, 0, 0, 0];
+        for (let i = 0; i < 7; i++) {
+            let gif = GIF();
+            gif.load(`/static/images/player/kyo/${i}.gif`);
+            this.animations.set(i, {
+                gif: gif,
+                frame_cnt: 0,
+                frame_rate: 10,
+                offset_y: offsets[i],
+                loaded: false,
+                scale: 2,
+            });
+
+            gif.onload = function() {
+                let obj = outer.animations.get(i);
+                obj.frame_cnt = gif.frames.length;
+                obj.loaded = true;
+            }
+            
+            if (i === 3) {
+                this.frame_rate = 4;
+            }
+        }
+    }
+}
